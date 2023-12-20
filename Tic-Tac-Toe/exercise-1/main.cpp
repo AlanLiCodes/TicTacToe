587da4856a1053b4a3302e42acd829c6d1a1f48b
Alan Li <44342646+AlanLiCodes@users.noreply.github.com>
2023-12-19 19:27:53 -0800

    Add files via upload

---
+++ Tic-Tac-Toe/exercise-1/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+#include <system_error>
+#include "GameState.h"
+using namespace std;
+
+//0 is player X
+//1 is play O
+
+int main(){
+    GameState game;
+
+    game.start();
+
+    return 0;
+}
